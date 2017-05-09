8c15c445771e0eff81f9b969505743b9952d01af
Gabriel Brickle <gabrielbrickle@gmail.com>
2017-05-09 17:14:17 -0400

    new content

--- js/custom.js
+++ js/custom.js
@@ -5,16 +5,16 @@
 	$(window).load(function(){
       $("#navigation").sticky({ topSpacing: 0 });
     });
-	
+
 
 
 	$('ul.nav li.dropdown').hover(function() {
 	  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
 	}, function() {
 	  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
-	});	
+	});
 
-	
+
 	//jQuery to collapse the navbar on scroll
 	$(window).scroll(function() {
 		if ($(".navbar").offset().top > 50) {
@@ -23,17 +23,23 @@
 			$(".navbar-fixed-top").removeClass("top-nav-collapse");
 		}
 	});
-	
+	$(".typed").typed({
+		strings: ["My Name is Gabriel", "I'm a Developer", "Let's build something awesome"],
+		typeSpeed: 75,
+		backDelay: 500,
+		// loop
+		loop: true
+});
 	//jQuery for page scrolling feature - requires jQuery Easing plugin
 	$(function() {
 		$('.navbar-nav li a').bind('click', function(event) {
 			var $anchor = $(this);
 			var nav = $($anchor.attr('href'));
 			if (nav.length) {
-			$('html, body').stop().animate({				
-				scrollTop: $($anchor.attr('href')).offset().top				
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
 			}, 1500, 'easeInOutExpo');
-			
+
 			event.preventDefault();
 			}
 		});
@@ -66,7 +72,7 @@
 	jQuery(".appear").on("appear", function(data) {
 			var id = $(this).attr("id");
 			jQuery('.nav li').removeClass('active');
-			jQuery(".nav a[href='#" + id + "']").parent().addClass("active");					
+			jQuery(".nav a[href='#" + id + "']").parent().addClass("active");
 		});
-	
+
 })(jQuery);
